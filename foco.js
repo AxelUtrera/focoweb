5246af2b6f3e29118817de33a736d439621e3a2d
Axel Utrera <axeljordanomu@gmail.com>
2023-10-23 21:41:06 -0600

    Se agrego un fondo bonito de efectoo para el foco

--- foco.js
+++ foco.js
@@ -7,16 +7,20 @@ function foco (){
     const btn = document.querySelector(".switch");
     const focoArriba = document.querySelector(".foco-arriba");
     const focoAbajo = document.querySelector(".foco-abajo");
+    const body = document.body;
 
     btn.addEventListener("click", function (){
         if(switchPrendido){
             btn.classList.remove("on");
             focoArriba.classList.remove("focoOn");
             focoAbajo.classList.remove("focoOn");
+            document.body.style.backgroundColor = "black"
         }else{
             btn.classList.add("on");
             focoArriba.classList.add("focoOn");
             focoAbajo.classList.add("focoOn");
+            
+            document.body.style.backgroundColor = "#ffff"
         }
 
         switchPrendido = !switchPrendido;
